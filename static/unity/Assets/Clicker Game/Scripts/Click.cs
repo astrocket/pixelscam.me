7a3e1873729bf12a1b18bbb64fb50fc844bc0719
hexoul <crosien@gmail.com>
2018-08-17 11:35:45 +0900

    Add clicker frame assets

---
+++ static/unity/Assets/Clicker Game/Scripts/Click.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Click : MonoBehaviour {
+
+	public UnityEngine.UI.Text goldDisplay;
+	public UnityEngine.UI.Text GPC;
+	public float gold = 0.00f;
+	public int goldperclick = 1;
+	public float costmultiplier = 2.00f;
+	void Awake()
+	{
+
+	}
+	void  Update()
+	{
+
+
+		goldDisplay.text = "Cookie: " + CurrencyManager.Instance.GetCurrencyIntoString(gold ,false ,false);
+		GPC.text = "CPC: " + CurrencyManager.Instance.GetCurrencyIntoString(goldperclick ,true ,false);
+	}
+	public void Clciked(){
+		gold += goldperclick;
+
+	}
+
+}
---
+++ static/unity/Assets/Clicker Game/Scripts/CurrencyManager.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CurrencyManager : MonoBehaviour {
+
+	private static CurrencyManager instance;
+	public static CurrencyManager Instance {
+		get {
+			return instance;
+		}
+	}
+
+	void Awake(){
+		CreateInstance ();
+	}
+
+
+		void CreateInstance(){
+		if (instance == null) {
+			instance = this;
+		}
+	}
+		
+	public string GetCurrencyIntoString(float valueToConvert, bool currencyPerSec, bool currencyPerClick ){
+		string converted;
+		if (valueToConvert >= 1000000000000000000) {
+			converted = (valueToConvert / 1000000000000000000f).ToString ("f2") + "Qui";
+		} else if (valueToConvert >= 1000000000000000) {
+			converted = (valueToConvert / 1000000000000000f).ToString ("f2") + "Qua";
+		} 
+		else if (valueToConvert >= 1000000000000) {
+			converted = (valueToConvert / 1000000000000f).ToString ("f2") + "T";
+		}
+		else if (valueToConvert >= 1000000000) {
+			converted = (valueToConvert / 1000000000f).ToString ("f2") + "B";
+		}
+		else if (valueToConvert >= 1000000) {
+			converted = (valueToConvert / 1000000f).ToString ("f2") + "M";
+		}
+		else if (valueToConvert >= 1000) {
+			converted = (valueToConvert / 1000f).ToString ("f2") + "K";
+		}
+
+		else {
+			converted = "" + valueToConvert;
+		}
+		if(currencyPerSec == true) {
+			converted = converted + " CPC";
+		}
+		if (currencyPerClick == true) {
+			converted = converted + " CPS";
+		}
+		return converted;
+	}
+}
