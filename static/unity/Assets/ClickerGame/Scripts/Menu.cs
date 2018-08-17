ee313d636e78cb5b30e07265f80e29f22005bc21
hexoul <crosien@gmail.com>
2018-08-17 14:29:36 +0900

    Change folder name, delete clicker tutorial, fix script error at Click.cs

---
+++ static/unity/Assets/ClickerGame/Scripts/Menu.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Menu : MonoBehaviour {
+
+
+	public void StartGame () {
+		Application.LoadLevel (1);
+	}
+	
+
+	public void Quit () {
+		Application.Quit();
+	}
+}
---
+++ static/unity/Assets/ClickerGame/Scripts/Pause.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pause : MonoBehaviour {
+
+		
+	void Update () {
+	if (Input.GetKey (KeyCode.Escape)) {
+			Application.LoadLevel(0);
+		}
+	}
+}
